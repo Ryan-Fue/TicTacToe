26ec5d35b155cb01dba29a7b080bf79a127a4943
Ryan Fue <ryanfue@Ryans-MacBook-Pro.local>
2025-02-13 17:46:49 -0800

    Completed Base Game

--- board.py
+++ board.py
@@ -26,21 +26,26 @@ class Board:
     
     @classmethod
     def generateBoard (cls):
+        # Clears terminal
         os.system('cls' if os.name == "nt" else 'clear')
 
-        if (cls.turn !=0) and (cls.turn %2 == 0):
+        # Updates board if not first turn or spot is taken
+        if (cls.turn !=0) and (cls.turn %2 == 1):
             cls.grid[cls.value-1] = " ❌"
-        elif (cls.turn !=0) and (cls.turn %2 == 1):
+        elif (cls.turn !=0) and (cls.turn %2 == 0):
             cls.grid[cls.value-1] = " ⭕️"
 
-
+        # Prints out the board
         for i in range(3):
             print(cls.grid[(i)*3]+"|"+cls.grid[(i)*3+1]+"|"+cls.grid[(i)*3+2])
             if i != 2:
                 print("-----------")
+        print("")
     
     @classmethod
     def startGame(cls):
+        cls.grid = [" 1 "," 2 "," 3 "," 4 "," 5 "," 6 "," 7 "," 8 "," 9 "]
+        cls.turn = 0
         Board.generateBoard()
         cls.turn = random.randint(1,2)
         print(f"Player{cls.markers[cls.turn%2]} goes first!")
@@ -61,16 +66,19 @@ class Board:
     def checkWin(cls):
         result = False
 
-
-
-
+        # Checks rows and columns
+        for i in range(3):
+            if (cls.grid[i*3] == cls.grid[i*3+1] == cls.grid[i*3+2]) \
+            or (cls.grid[i] == cls.grid[i+3] == cls.grid[i+6]):
+                result = True
+        
+        # Checks diagonals
+        if (cls.grid[0] == cls.grid[4] == cls.grid[8]) \
+            or (cls.grid[2] == cls.grid[4] == cls.grid[6]):
+                result = True
 
         return result
     
-
-    
-
-
 
         
 
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from board import *
 
 def main():
-    Board.startGame()
+    while True:
+        Board.startGame()
+        while True:
+            choice = input("Play again?(Y/N) ")
+            if choice == "N":
+                return
+            elif choice == "Y":
+                break
+            else:
+                print("That is not Y or N!!!")
+            
+
 
 
 if __name__ == "__main__":
